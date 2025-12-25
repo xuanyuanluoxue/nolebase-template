92a44475eff7ceb99013746411df20fb09af8a6e
xuanyuanluoxue <3229079026@qq.com>
2025-12-25 22:24:42 +0800

    update name2

--- metadata/index.ts
+++ metadata/index.ts
@@ -1,6 +1,6 @@
 /** 文本 */
-export const siteName = 'Nólëbase'
-export const siteShortName = 'Nólëbase'
+export const siteName = 'xavier'
+export const siteShortName = 'xavier'
 export const siteDescription = '记录回忆，知识和畅想的地方'
 
 /** 文档所在目录 */
